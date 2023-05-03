532c05df614acbc3844e07fb3ebb8884c1983b5c
Chad.Gojmerac <chad.gojmerac@ahead.com>
2023-05-03 16:37:56 -0700

    Changed to better fit standards added terraform-docs readme output.

--- main.tf
+++ main.tf
@@ -22,23 +22,3 @@ module "vpc" {
     var.tags,
   )
 }
-
-output "vpc" {
-  value = module.vpc
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
-
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
-
-output "database_subnets" {
-  value = module.vpc.database_subnets
-}
